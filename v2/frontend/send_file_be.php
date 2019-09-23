d5c7d2427415701d9d81b81eaf3cfd43842871d9
yurisa2 <yuri@sa2.com.br>
2019-09-23 17:07:40 -0300

    Hard Rule Size

--- v2/frontend/send_file_be.php
+++ v2/frontend/send_file_be.php
@@ -68,7 +68,7 @@ if ($uploadOk == 0) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
 
-      $files = (fsplit($target_file,$target_dir_split,1024));
+      $files = (fsplit($target_file,$target_dir_split,512000));
 
     } else {
         echo "Sorry, there was an error uploading your file.";
